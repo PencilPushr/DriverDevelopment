9cece9e47fae8aaa5c8fe2919c04aaa095b5107b
AJ <ajvallee15@gmail.com>
2024-10-29 11:27:52 +0000

    Initial commit

---
+++ Driver.cpp
@@ -0,0 +1,31 @@
+﻿#include <ntddk.h>
+
+// Function declarations
+extern "C" {
+    DRIVER_INITIALIZE DriverEntry;
+    DRIVER_UNLOAD DriverUnload;
+}
+
+// Main driver entry point
+extern "C" NTSTATUS DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
+)
+{
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    // Set up driver unload routine
+    DriverObject->DriverUnload = DriverUnload;
+
+    DbgPrint("Kernel Driver: Driver loaded\n");
+    return STATUS_SUCCESS;
+}
+
+// Driver unload routine
+void DriverUnload(
+    _In_ PDRIVER_OBJECT DriverObject
+)
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    DbgPrint("Kernel Driver: Driver unloaded\n");
+}
